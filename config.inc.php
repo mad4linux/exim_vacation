ec441cf77b67b66e7da27f2ae48d4b074f95286e
Daniel Savi <gaess@gmx.net>
2015-05-20 23:26:11 +0200

    more general config options for github

--- config.inc.php
+++ config.inc.php
@@ -7,8 +7,8 @@
 //
 
 // path to mailboxes directory
-$rcmail_config['exim_vacation_mailboxes_root'] = '/home/mailaccounts/';
-$rcmail_config['exim_vacation_subfolder'] = 'vacation_msg';
+$rcmail_config['exim_vacation_mailboxes_root'] = '/home/';
+$rcmail_config['exim_vacation_subfolder'] = '.vacation_msg';
 $rcmail_config['exim_vacation_msg_active'] = 'vacation.msg';
 $rcmail_config['exim_vacation_msg_passive'] = 'vacation.no';
 $rcmail_config['exim_vacation_recipients_db'] = 'vacationRecipients.lst';
